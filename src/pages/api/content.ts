aea296daa1580c7d0dbbabaa493eafcb1688fe5e
benl95 <beno_1995@hotmail.com>
2021-11-24 23:45:11 +0100

    feat: create content endpoint

---
+++ src/pages/api/content.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Sections } from '../../types/sections';
+import * as content from '../../content/content.json';
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Sections>
+) {
+    try {
+        res.status(200).json(content);
+    } catch (err) {
+        throw err;
+    }
+}
