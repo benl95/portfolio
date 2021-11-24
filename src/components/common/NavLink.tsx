794ce6614fb5746867a8564ec4d23a45a5093fc3
benl95 <beno_1995@hotmail.com>
2021-11-24 23:50:35 +0100

    chore: update path LinkType

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { LinkType } from '../../types/link';
+import { LinkType } from '../../types/navigation';
 
 const NavLink = ({ url, text }: LinkType) => {
-	return <a href={url}>{text}</a>;
+    return <a href={url}>{text}</a>;
 };
 
 export default NavLink;
