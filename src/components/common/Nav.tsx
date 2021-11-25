e2fc7f772e98571c440ea2831ccb862876b3d7ed
benl95 <beno_1995@hotmail.com>
2021-11-26 00:21:35 +0100

    refactor: move navigation data to component level

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import Link from '@/components/Link';
-import { Navigation } from '@/types/navigation';
+import { LinkType } from '@/types/link';
 
-const Nav = ({ links }: Navigation) => {
-    const navLinks = links.map(({ url, text }) => {
+const Nav = () => {
+    const links = [
+        {
+            url: '/home/#about-me',
+            text: 'About me',
+        },
+        {
+            url: '/home/#projects',
+            text: 'Projects',
+        },
+        {
+            url: '/home/#contact',
+            text: 'Contact',
+        },
+        {
+            url: '/api/resume',
+            text: 'Resume',
+        },
+    ];
+    const navLinks = links.map(({ url, text }: LinkType) => {
         return (
             <li key={text}>
                 <Link url={url} text={text} />
