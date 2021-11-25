aad990f56be77dd9926bf9b3699e5f3238bd6578
benl95 <beno_1995@hotmail.com>
2021-11-26 00:21:13 +0100

    feat: create Layout

---
+++ src/components/templates/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Nav from '@/components/Nav';
+import Head from 'next/head';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            <Head>
+                <title>Ben Langenberg</title>
+                <meta
+                    name="description"
+                    content="Ben Langenberg is a developer who is specialized in designing and building accessible web applications."
+                />
+            </Head>
+            <Nav />
+            {children}
+        </>
+    );
+};
+
+export default Layout;
