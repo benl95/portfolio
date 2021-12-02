ae020c052c911cbf1e6bde2b1d47626bcd4e8ca1
benl95 <beno_1995@hotmail.com>
2021-12-02 23:30:51 +0100

    styles: dynamic font-size, opacity and text-decoration

--- src/components/styles/Link.styles.ts
+++ src/components/styles/Link.styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import { DimensionProps } from '@/types/theme';
+import { handleFontSize, handleContrastLevel } from '@/utils/styleAttributes';
 
 export const StyledLink = styled('a')<DimensionProps>`
-    text-decoration: none;
+    font-size: ${({ size }: DimensionProps): string[] => handleFontSize(size)};
+    opacity: ${({ contrast }: DimensionProps): number[] =>
+        handleContrastLevel(contrast)};
+    text-decoration: ${({ underline }) =>
+        underline === true ? 'underline' : 'none'};
 `;
