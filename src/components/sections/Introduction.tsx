b7536321338178b6cf03cf851e8579a6af8a6cfe
benl95 <beno_1995@hotmail.com>
2021-12-02 14:11:21 +0100

    chore: remove passed props

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -2,25 +2,12 @@ import React, { Component } from 'react';
 import Section from '@/components/Section';
 import Paragraph from '@/components/Paragraph';
 import { AboutMe } from '@/types/content';
-import {
-    ParagraphSizeMap,
-    TitleSizeMap,
-    ContrastLevels,
-} from '@/types/StyleMaps';
 
 const Introduction = ({ title, name, occupation, about }: AboutMe) => {
     return (
-        <Section title={title} size={TitleSizeMap.s}>
-            <Paragraph
-                text={name}
-                size={ParagraphSizeMap.l}
-                contrast={ContrastLevels.high}
-            />
-            <Paragraph
-                text={occupation}
-                size={ParagraphSizeMap.l}
-                contrast={ContrastLevels.high}
-            />
+        <Section title={title}>
+            <Paragraph text={name} />
+            <Paragraph text={occupation} />
             <Paragraph text={about} />
         </Section>
     );
