7444bdd66efb1fac1fc97d40e33e3652c000ee33
benl95 <beno_1995@hotmail.com>
2021-12-02 23:23:15 +0100

    chore: link to repo in footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import Link from '@/components/Link';
 
 const Footer = () => {
-    return <footer>Designed & Built by Ben Langenberg</footer>;
+    const text = 'Designed & Built by Ben Langenberg';
+    const url = 'https://github.com/benl95/portfolio';
+    return (
+        <footer>
+            <Link url={url} text={text} contrast="high" />
+        </footer>
+    );
 };
 
 export default Footer;
