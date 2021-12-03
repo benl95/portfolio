f6c10849a730e3f958e8d3be07610de634fa5346
benl95 <beno_1995@hotmail.com>
2021-12-03 14:44:05 +0100

    chore: add font weight to themes

--- src/components/styles/Theme.styles.ts
+++ src/components/styles/Theme.styles.ts
@@ -2,7 +2,7 @@ import { ThemeProps } from '@/types/theme';
 
 const fontSizes = ['1rem', '1.875rem', '1.5rem', '2.813rem', '2.5rem'];
 const contrastLevels = [0.6, 0.87];
-const fontWeights = [300, 500];
+const fontWeights = [300, 500, 800];
 const spacing = ['2.25rem', '1.313rem'];
 
 const colors = {
