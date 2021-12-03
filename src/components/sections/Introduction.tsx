77d6cbdc640f9bcbc97ad23b528db664fb65e5e6
benl95 <beno_1995@hotmail.com>
2021-12-03 02:35:49 +0100

    chore: structure dom

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import Section from '@/components/Section';
-import Paragraph from '@/components/Paragraph';
 import { AboutMe } from '@/types/content';
+import Section from '@/components/Section';
+import Link from '@/components/Link';
+import Paragraph from '@/components/Paragraph';
+import Image from 'next/image';
 
 const Introduction = ({ title, name, occupation, about }: AboutMe) => {
     return (
-        <Section title={title}>
-            <Paragraph text={name} />
-            <Paragraph text={occupation} />
-            <Paragraph text={about} />
+        <Section title={title} contrast="low" size="xl">
+            <Paragraph text={name} size="xxl" contrast="high" />
+            <Paragraph text={occupation} size="xxl" />
+
+            <ul>
+                <li>
+                    <Image
+                        src="/github-icon.svg"
+                        alt="Github icon"
+                        width={30}
+                        height={30}
+                    />
+                </li>
+                <li>
+                    <Image
+                        src={'/linkedin-icon.svg'}
+                        alt="LinkedIn icon"
+                        width={30}
+                        height={30}
+                    />
+                </li>
+                <li>
+                    <Link
+                        url={'mailto:ben.langenberg987@gmail.com'}
+                        text="hello@ben.com"
+                    />
+                </li>
+            </ul>
         </Section>
     );
 };
