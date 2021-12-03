2592a328614454cf191bfbcd3731c44418c7c1ab
benl95 <beno_1995@hotmail.com>
2021-12-03 02:43:04 +0100

    chore: remove container wrapper for now

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Children } from '@/types/content';
-import Container from '@/components/Container';
-import { StyledSection } from '@/styles/Section.styles';
+import { StyledSection } from 'components/styles/Section.styles';
 import { DimensionProps } from '@/types/theme';
 
 interface SectionProps extends Children {
@@ -14,7 +13,8 @@ const Section = ({ title, size, contrast, children }: Props) => {
     return (
         <StyledSection size={size} contrast={contrast}>
             <h2>{title}</h2>
-            <Container>{children}</Container>
+            {/* Maybe we can add a functionality here, to optionally wrap the content in a container when the feat is eventually needed.  */}
+            {children}
         </StyledSection>
     );
 };
