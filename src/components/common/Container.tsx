be9d06e652c366b07463dc41b79c768ea89b6bcb
benl95 <beno_1995@hotmail.com>
2021-12-03 02:39:05 +0100

    feat: add className prop to container component

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Children } from '@/types/content';
 
-const Container = ({ children }: Children) => {
-    return <div>{children}</div>;
+interface className {
+    className?: string;
+}
+type Props = className & Children;
+
+const Container = ({ className, children }: Props) => {
+    return <div className={className}>{children}</div>;
 };
 
 export default Container;
