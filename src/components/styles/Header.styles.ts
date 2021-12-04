855a7c27d45347f832d531e921e6cecd07781e68
benl95 <beno_1995@hotmail.com>
2021-12-04 10:47:53 +0100

    chore: remove styled components modules

--- src/components/styles/Header.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const NavContainer = styled('div')`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    height: 5.688rem;
-    padding: 36px 21px 36px 21px;
-`;
