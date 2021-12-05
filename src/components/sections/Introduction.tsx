029aab0807e21e74790db131173ffd8919233487
benl95 <beno_1995@hotmail.com>
2021-12-05 01:15:55 +0100

    chore: wrap links list

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -10,29 +10,31 @@ const Introduction = ({ title, name, occupation }: AboutMe) => {
                 <p>{name}</p>
                 <p>{occupation}</p>
             </div>
-            <ul>
-                <li>
-                    <Image
-                        src="/github-icon.svg"
-                        alt="Github icon"
-                        width={30}
-                        height={30}
-                    />
-                </li>
-                <li>
-                    <Image
-                        src={'/linkedin-icon.svg'}
-                        alt="LinkedIn icon"
-                        width={30}
-                        height={30}
-                    />
-                </li>
-                <li>
-                    <a href={'mailto:ben.langenberg987@gmail.com'}>
-                        hello@ben.com
-                    </a>
-                </li>
-            </ul>
+            <div>
+                <ul>
+                    <li>
+                        <Image
+                            src="/github-icon.svg"
+                            alt="Github icon"
+                            width={30}
+                            height={30}
+                        />
+                    </li>
+                    <li>
+                        <Image
+                            src={'/linkedin-icon.svg'}
+                            alt="LinkedIn icon"
+                            width={30}
+                            height={30}
+                        />
+                    </li>
+                    <li>
+                        <a href={'mailto:ben.langenberg987@gmail.com'}>
+                            hello@ben.com
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </section>
     );
 };
