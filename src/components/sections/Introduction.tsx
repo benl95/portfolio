2638578b64df6dd4d568973d16f638191a37088f
benl95 <beno_1995@hotmail.com>
2021-12-05 01:47:53 +0100

    styles: style header for mobile

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import Image from 'next/image';
+import styles from './Introduction.module.css';
 import { AboutMe } from '@/types/content';
 
 const Introduction = ({ title, name, occupation }: AboutMe) => {
     return (
-        <section>
-            <div>
-                <h2 title={title}>{title}</h2>
-                <p>{name}</p>
-                <p>{occupation}</p>
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <Image
-                            src="/github-icon.svg"
-                            alt="Github icon"
-                            width={30}
-                            height={30}
-                        />
-                    </li>
-                    <li>
-                        <Image
-                            src={'/linkedin-icon.svg'}
-                            alt="LinkedIn icon"
-                            width={30}
-                            height={30}
-                        />
-                    </li>
-                    <li>
-                        <a href={'mailto:ben.langenberg987@gmail.com'}>
-                            hello@ben.com
-                        </a>
-                    </li>
-                </ul>
+        <section className={styles.Section}>
+            <div className={styles.HeaderContainer}>
+                <div className={styles.IntroductionContainer}>
+                    <h2 className={styles.Title} title={title}>
+                        {title}
+                    </h2>
+                    <p className={styles.Bold}>{name}</p>
+                    <p className={`${styles.Bold} ${styles.LowContrast}`}>
+                        {occupation}
+                    </p>
+                </div>
+                <div>
+                    <ul className={styles.LinksContainer}>
+                        <li>
+                            <Image
+                                src="/github-icon.svg"
+                                alt="Github icon"
+                                width={30}
+                                height={30}
+                            />
+                        </li>
+                        <li className={styles.FlexGrow}>
+                            <Image
+                                src={'/linkedin-icon.svg'}
+                                alt="LinkedIn icon"
+                                width={30}
+                                height={30}
+                            />
+                        </li>
+                        <li className={styles.MarginTop}>
+                            <a href={'mailto:ben.langenberg987@gmail.com'}>
+                                hello@ben.com
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     );
