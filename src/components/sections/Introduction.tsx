a6cdda09aff0b741d561b9f4b58292efdf5d14da
benl95 <beno_1995@hotmail.com>
2021-12-10 15:07:08 +0100

    styles: position header content, display sidelinks on breakpoint

--- src/components/sections/Introduction.tsx
+++ src/components/sections/Introduction.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './Introduction.module.css';
+import SideLink from '@/components/links/SideLink';
 import { AboutMe } from '@/types/content';
 
 const Introduction = ({
@@ -12,6 +13,30 @@ const Introduction = ({
 }: AboutMe) => {
     return (
         <section className={styles.Section}>
+            <SideLink orientation="left">
+                <ul className={styles.Flex}>
+                    <li>
+                        <a href={linkedIn} className={styles.HighContrast}>
+                            <Image
+                                src={'/linkedin-icon.svg'}
+                                alt="LinkedIn icon"
+                                width={30}
+                                height={30}
+                            />
+                        </a>
+                    </li>
+                    <li>
+                        <a href={github} className={styles.HighContrast}>
+                            <Image
+                                src="/github-icon.svg"
+                                alt="Github icon"
+                                width={30}
+                                height={30}
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </SideLink>
             <div className={styles.HeaderContainer}>
                 <div className={styles.IntroductionContainer}>
                     <h2 className={`${styles.Title}`} title={title}>
@@ -22,7 +47,7 @@ const Introduction = ({
                         {occupation}
                     </p>
                 </div>
-                <div>
+                <div className={styles.Breakpoint}>
                     <ul className={styles.LinksContainer}>
                         <li>
                             <a href={github} className={styles.HighContrast}>
@@ -55,6 +80,16 @@ const Introduction = ({
                     </ul>
                 </div>
             </div>
+            <SideLink orientation="right">
+                <div>
+                    <a
+                        className={styles.Vertical}
+                        href={'mailto:ben.langenberg987@gmail.com'}
+                    >
+                        hello@ben.com
+                    </a>
+                </div>
+            </SideLink>
         </section>
     );
 };
