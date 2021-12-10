3ab8b486a11ad70c413b7a9726bb6de7343aa24f
benl95 <beno_1995@hotmail.com>
2021-12-10 21:56:01 +0100

    feat: show hamburger menu onClick

--- src/components/common/nav/MobileNav.tsx
+++ src/components/common/nav/MobileNav.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './MobileNav.module.css';
+import NavLinks from './NavLinks';
+import ImageButton from './ImageButton';
+import { useState } from 'react';
 interface Props {
     text: string;
 }
@@ -9,23 +12,29 @@ const MobileNav = ({ text }: Props) => {
     const width: number = 37;
     const height: number = 25;
 
+    const [click, setClick] = useState(false);
+
     return (
         <nav>
             <div className={styles.NavContainer}>
-                <ul className={styles.Nav}>
-                    {/* TODO: pass src and text as props */}
-                    <li>
-                        <a href={'http://localhost:3000'}>{text}</a>
-                    </li>
-                    <li>
-                        <Image
-                            src="/hamburger.svg"
-                            alt="Hamburger menu icon"
-                            width={width}
-                            height={height}
-                        />
-                    </li>
-                </ul>
+                <a href={'http://localhost:3000'}>{text}</a>
+                <div>
+                    <ImageButton
+                        src="/hamburger.svg"
+                        alt="Hamburger menu icon"
+                        width={width}
+                        height={height}
+                        onClick={() => setClick(!click)}
+                    />
+
+                    {click && (
+                        <div className={styles.Hamburger}>
+                            <NavLinks
+                                links={['introduction', 'projects', 'contact']}
+                            />
+                        </div>
+                    )}
+                </div>
             </div>
         </nav>
     );
